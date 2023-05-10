080eeb99063453473068feeb875b99a0785ccf1f
unknown <you@example.com>
2023-05-10 17:20:58 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("Hola")
+
+let nombreAlumno = prompt("ingrese su nombre")
+alert ("Hola"+" "+nombreAlumno)
+let Año = Number(prompt("¿En que año estamos?"))
+
+
+let edad =  prompt("ingresa tu edad")
+if (edad >=18){
+console.log("soy mayor a 18") 
+    
+}else{
+console.log("soy menor de edad") 
+}
+
