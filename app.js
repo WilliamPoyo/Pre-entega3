e3e14986f852b1aa197481f2073eeca5da431bda
unknown <you@example.com>
2023-05-17 16:57:51 -0300

    cambio

--- app.js
+++ app.js
@@ -12,41 +12,41 @@ nombre()
 function años() {
     
 
-let edad =  prompt("ingresa tu edad")
-if (edad >=18){
-console.log("soy mayor a 18") 
+let ValidarEdad =  prompt("ingresa tu edad")
+if (ValidarEdad >=18){
+alert("soy mayor a 18") 
     
 }else{
-console.log("soy menor de edad") 
+alert("soy menor de edad") 
 }}
 años()
-function Beber() {
+function Bebidas() {
     
 
 let bebida = prompt("¿Que bebida queres?")
 switch(bebida){
     case "fernet":
-    console.log(`pedido${bebida},
+    alert(`pedido${bebida},
     valor 2000`)
     break
     case "Cerveza":
-        console.log(`pedido${bebida},
+    alert(`pedido${bebida},
         valor 1000`)
         break
         case"Ron":
-        console.log(`pedido${bebida},
+        alert(`pedido${bebida},
         valor 2200`)
         break
         case "whisky":
-            console.log(`pedido${bebida},
+            alert(`pedido${bebida},
             valor 2200`)
         break
         case "vodka":
-        console.log(`pedido${bebida},
+        alert(`pedido${bebida},
         valor 2200`)
         break
         default:
-            console.log("Ninguna opcion anterior")
+            alert("Ninguna opcion anterior")
             break
         }
 }
